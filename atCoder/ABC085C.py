24785d74ffa1aafc98f310f8dcc7ab52f56c5258
asuyzk <asuyzk.biz@gmail.com>
2023-10-31 22:15:38 +0900

    20231031

--- atCoder/ABC085C.py
+++ atCoder/ABC085C.py
@@ -1,19 +1,31 @@
 inVal = list(map(int,list(input().split())))
+numN = inVal[0]
 numY = inVal[1]
 
 bills = [10000, 5000, 1000]
-factBills = [0, 0, 0]
-
+divBills = [0, 0, 0]
 for cnt in range(len(bills)):
-  while True:
-    if numY >= bills[cnt]:
-      print(numY)
-      factBills[cnt] += 1
-      numY = numY - bills[cnt]
+  divBills[cnt] = numY // bills[cnt]
+
+cntBills = [0, 0, 0]
+factBills = [-1, -1, -1]
+for cnt1k in range(divBills[2] + 1):
+  for cnt5k in range(divBills[1] + 1):
+    for cnt10k in range(divBills[0] + 1):
+      cntBills = [cnt10k, cnt5k, cnt1k]
+
+      if sum(cntBills) == numN:
+        amount = [bills[i] * cntBills[i] for i in range(3)]
+        # print(str(cntBills) + str(sum(amount)))
+
+        if sum(amount) == numY:
+          factBills = cntBills
+          break
     else:
-      break
-  
-  if inVal[0] != sum(factBills):
-    factBills = [-1, -1, -1]
-  
+      continue
+    break
+  else:
+    continue
+  break
+
 print(*factBills)
