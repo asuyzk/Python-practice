cc485eafd31d9bb6e45cb2ada8d7f65ff4bfb3d1
asuyzk <asuyzk.biz@gmail.com>
2023-11-13 21:18:24 +0900

    20231113

--- atCoder/ABC086C.py
+++ atCoder/ABC086C.py
@@ -1,15 +1,18 @@
 numN = int(input())
-timeT = ["0 0 0"] + [input() for cntIn in range(numN)]
+plan = ["0 0 0"] + [input() for cntIn in range(numN)]
 
 flag = 0
 for cntN in range(1, numN + 1):
-  behindP = list(map(int, timeT[cntN-1].split()))
-  curtP = list(map(int, timeT[cntN].split()))
+  behindP = list(map(int, plan[cntN-1].split()))
+  curtP = list(map(int, plan[cntN].split()))
 
   if curtP[1] == behindP[1] and curtP[2] == behindP[2]:
     flag = 1
     break
   elif curtP[0] < curtP[1] + curtP[2]:
+    flag = 1
+    break
+  elif curtP[0] - 1 == curtP[1] + curtP[2]:
     flag = 1
     break
 
